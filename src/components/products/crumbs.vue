fff9ca88ec055f7f010db511e2a90ace28b788ce
yangxiaoqiam <2391627812@qq.com>
2019-05-09 17:16:31 +0800

    add

--- src/components/products/crumbs.vue
+++ src/components/products/crumbs.vue
@@ -1,7 +1,7 @@
 <template>
   <el-breadcrumb separator-class="el-icon-arrow-right">
-    <el-breadcrumb-item>{{$store.state.position}}美团</el-breadcrumb-item>
-    <el-breadcrumb-item>{{$store.state.position}}{{$route.params.name}}</el-breadcrumb-item>
+    <el-breadcrumb-item>{{$store.state.position.name}}美团</el-breadcrumb-item>
+    <el-breadcrumb-item>{{$route.params.name}}</el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 <script>
